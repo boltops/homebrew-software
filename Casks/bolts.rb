634636713fc0a0615e501e74b89e6a9011731393
Tung Nguyen <tongueroo@gmail.com>
2017-07-13 10:17:00 -0700

    update bolts package

--- Casks/bolts.rb
+++ Casks/bolts.rb
@@ -1,8 +1,8 @@
 # This file is generated. To update it, run rake release:update
 cask 'bolts' do
-  version '1.2.1'
-  sha256 'ba1f7cabbf2879f03c25958ac6822a68ca856146ccad8f1333396efa385acb58'
-  package_name = 'bolts-1.2.1-1.dmg'
+  version '1.2.2'
+  sha256 'e9fe4f8539aebc5a86d9682da2fb64af02a4153e6a599508f8f3ceca5d9175b5'
+  package_name = 'bolts-1.2.2-1.dmg'
 
   url "https://s3-us-west-2.amazonaws.com/packages.boltops.com/bolts/#{version}/#{package_name}/#{package_name}"
   name 'Bolts'
