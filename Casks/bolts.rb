c84a61f422a83f90c7cfabe08156c9892567445d
Tung Nguyen <tongueroo@gmail.com>
2018-04-20 23:12:43 -0700

    update bolts package for macosx

--- Casks/bolts.rb
+++ Casks/bolts.rb
@@ -1,6 +1,6 @@
 # This file is generated. To update it, run rake release:update
 cask 'bolts' do
-  version '1.7.1+20180421055212'
+  version '1.7.1'
   sha256 '5f172bbc9c9237b694c57bf737af48f79725fbe2be5309e7f8fc5fcd8f91a88e'
   package_name = 'bolts-1.7.1+20180421055212-2.dmg'
 
