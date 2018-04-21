605fbf20c485cc2888a7e9c0c9fa008d9ccb2cca
Tung Nguyen <tongueroo@gmail.com>
2018-04-20 23:20:52 -0700

    update bolts package for macosx

--- Casks/bolts.rb
+++ Casks/bolts.rb
@@ -4,7 +4,7 @@ cask 'bolts' do
   sha256 '5f172bbc9c9237b694c57bf737af48f79725fbe2be5309e7f8fc5fcd8f91a88e'
   package_name = 'bolts-1.7.1+20180421055212-2.dmg'
 
-  url "https://packages.boltops.com/bolts/#{version}/#{package_name}/#{package_name}"
+  url "http://packages.boltops.com/bolts/#{version}/#{package_name}/#{package_name}"
   name 'Bolts'
   homepage 'https://boltops.com/toolkit'
 
